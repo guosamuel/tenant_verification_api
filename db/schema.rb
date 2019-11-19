232494ef3277b3fc89ac3cdd43dfca088fb1d080
github username <github email address>
2019-11-19 01:15:12 +0100

    table name successfully changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_202212) do
+ActiveRecord::Schema.define(version: 2019_11_19_001201) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street1"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_11_12_202212) do
     t.string "city"
     t.string "state"
     t.integer "postal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "landlords", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -36,15 +45,6 @@ ActiveRecord::Schema.define(version: 2019_11_12_202212) do
   create_table "tenants", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
