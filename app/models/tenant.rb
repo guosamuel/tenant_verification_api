180a480c407bf6839ed931821912d80f9b615817
github username <github email address>
2019-11-25 13:41:08 -0500

    added tenant model validations

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,4 +1,6 @@
 class Tenant < ApplicationRecord
     has_many :reviews
     has_many :landlords, :through => :reviews
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 end
