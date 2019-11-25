c2b258985a4101430deb9b033081a3e9f3bb1eb1
Ashley <ashleywong678@gmail.com>
2019-11-25 11:46:57 -0500

    added validations for all attributes except end_date

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ApplicationRecord
   belongs_to :landlord
   belongs_to :tenant
 
+  validates :landlord, :tenant, :start_date, :address, :comment, presence: true
+  validates :comment, length: { minimum: 2 }
+
 end
