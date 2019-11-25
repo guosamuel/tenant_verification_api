fe3fc13572a1d82cd240038aa46129b025758027
Kevin Wang <kcwang06@gmail.com>
2019-11-25 17:22:06 -0500

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,6 @@ class Address < ApplicationRecord
     belongs_to :landlord   
 
     validates :street1, :city, :state, :postal, presence: true
-    validates :postal, length: { in: 5..9}, numericality: {only_integer: true}
+    validates :postal, length: { in: 5..9}
 
 end
