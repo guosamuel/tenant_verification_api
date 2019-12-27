c02ca2a991c102fa81a757ca4bbeca6156d8685a
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-12-27 16:27:43 -0500

    completed the create method

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -6,7 +6,14 @@ class LandlordsController < ApplicationController
   end
 
   def create
-
+    # param keys may subject to change depending on the body of the post request
+    new_landlord = Landlord.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
+    if new_landlord.valid?
+      Landlord.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
+      render json: {message: "Landlord successfully created"}
+    else
+      render json: {message: "There was an error."}
+    end
   end
 
   def update
