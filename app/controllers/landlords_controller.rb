af57c6b3b9df32cb37865dc7d91280e0e172afca
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2019-12-27 17:30:56 -0500

    completed the update method

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -10,14 +10,25 @@ class LandlordsController < ApplicationController
     new_landlord = Landlord.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
     if new_landlord.valid?
       Landlord.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
+      # message can be altered for better UI experience in the future
       render json: {message: "Landlord successfully created"}
     else
+      # message can be altered for better UI experience in the future
       render json: {message: "There was an error."}
     end
   end
 
   def update
-
+    id = params[:id]
+    if Landlord.update(id, first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password]).valid?
+      Landlord.update(id, first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
+      updated_landlord = Landlord.find(id)
+      # message can be altered for better UI experience in the future
+      render json: {landlord: updated_landlord, message: "Landlord successfully updated"}
+    else
+      # message can be altered for better UI experience in the future
+      render json: {message: "Update failed"}
+    end
   end
 
   def delete
