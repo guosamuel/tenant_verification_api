d57e67f975183d3a92cee39951259d6f0c491379
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-01-03 18:52:15 -0500

    able to get nested routes for creating

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -19,14 +19,28 @@ class TenantsController < ApplicationController
 
   def create
     # param keys may subject to change depending on the body of the post request
-    new_tenant = Tenant.new(first_name: params[:first_name], last_name: params[:last_name])
-    if new_tenant.valid?
-      Tenant.create(first_name: params[:first_name], last_name: params[:last_name])
-      # message can be altered for better UI experience in the future
-      render json: {message: "Tenant successfully created"}
+    if !params[:landlord_id]
+      new_tenant = Tenant.new(first_name: params[:first_name], last_name: params[:last_name])
+      if new_tenant.valid?
+        Tenant.create(first_name: params[:first_name], last_name: params[:last_name])
+        # message can be altered for better UI experience in the future
+        render json: {message: "Tenant successfully created"}
+      else
+        # message can be altered for better UI experience in the future
+        render json: {message: "There was an error."}
+      end
     else
-      # message can be altered for better UI experience in the future
-      render json: {message: "There was an error."}
+      new_tenant = Tenant.new(first_name: params[:first_name], last_name: params[:last_name])
+      if new_tenant.valid?
+        Tenant.create(first_name: params[:first_name], last_name: params[:last_name])
+        # all inputs for creating the Review are default values. Only the landlord_id and tenant_id are actual correct values. MUST BE UPDATED!!! Needed the default values to pass Review validations
+        Review.create(start_date: Date.new(2000, 1, 1), end_date: Date.new(2000, 1, 1), landlord_id: params[:landlord_id], tenant_id: Tenant.last.id, address: "TBD", comment: "TBD")
+        # message can be altered for better UI experience in the future
+        render json: {message: "Tenant successfully created"}
+      else
+        # message can be altered for better UI experience in the future
+        render json: {message: "There was an error."}
+      end
     end
   end
 
