38aec26dd188bfe0f4849538b10efe5ba5e87fe4
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-01-03 17:02:06 -0500

    added seed data for testing to figure out how to do the nested routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 Landlord.find_or_create_by(first_name: "Sam", last_name: "Guo", email: "derp@gmail.com", password_digest: '123')
 Tenant.find_or_create_by(first_name: "Nancy", last_name: "Lin")
+Review.find_or_create_by(start_date: Date.new(2020, 1, 1), end_date: Date.new(2020, 1, 2), landlord_id: 2, tenant_id: 4, address: "123 Fake St", comment: "Testing right now")
