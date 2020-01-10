44a45c3d197af6059f8c001522f005055b483a99
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-01-10 12:07:16 -0500

    helped Jos troubleshooting the seeed data. she pushed it up for review now

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Landlord.find_or_create_by(first_name: "Sam", last_name: "Guo", email: "derp@gmail.com", password_digest: '123')
-Tenant.find_or_create_by(first_name: "Nancy", last_name: "Lin")
-Review.find_or_create_by(start_date: Date.new(2020, 1, 1), end_date: Date.new(2020, 1, 2), landlord_id: 2, tenant_id: 4, address: "123 Fake St", comment: "Testing right now")
+for x in 1..6
+    Landlord.create(first_name: "Sam#{x}", last_name: "Guo#{x}", email: "derp#{x}@gmail.com", password: '123#{x}')
+    Tenant.find_or_create_by(first_name: "Nancy#{x}", last_name: "Lin#{x}")
+    Review.find_or_create_by(start_date: Date.new(2020, x+1, x+1), end_date: Date.new(2020, x+1, x+2), landlord_id: x, tenant_id: x+4, address: "123#{x} Fake St", comment: "Testing#{x} right now")
+end
+
+# Landlord.find_or_create_by(first_name: "Sam", last_name: "Guo", email: "derp@gmail.com", password: '123')
+# Review.new(start_date: Date.new(2020, 2, 2), end_date: Date.new(2020, 2, 3), landlord_id: 1, tenant_id: 5, address: "1232 Fake St", comment: "Testing1 right now")
+Landlord.create(first_name: "Sam1", last_name: "Guo1", email: "derp1@gmail.com", password_digest: '123#{x}')
+# Tenant.find_or_create_by(first_name: "Nancy1", last_name: "Lin1")
+# Review.find_or_create_by(start_date: Date.new(2020, 2, 2), end_date: Date.new(2020, 2, 3), landlord_id: 1, tenant_id: 5, address: "1231 Fake St", comment: "Testing1 right now")
